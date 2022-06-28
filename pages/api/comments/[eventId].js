10b052b537bee5628ab0ea9607238ee65fff5721
AndyPew <christianandyp09@gmail.com>
2022-06-28 15:15:18 +0700

    error handling & mongoDB

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,36 +1,51 @@
-export default function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
   const eventId = req.query.eventId;
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://maxmilian:asolole1234@clusters.vboii.mongodb.net/events?retryWrites=true&w=majority"
+  );
 
   if (req.method === "POST") {
     const { email, name, text } = req.body;
 
     if (
-      !email.include("@" || !name || name.trim() === "" || text.trim() === "")
+      !email.includes("@" || !name || name.trim() === "" || text.trim() === "")
     ) {
-        res.status(422).json({message: 'Invalid Input'})
-        return
+      res.status(422).json({ message: "Invalid Input" });
+      return;
     }
 
     const newComment = {
-        id: new Date().toISOString,
-        email,
-        name,
-        text
-    }
+      email,
+      name,
+      text,
+      eventId,
+    };
 
-    console.log(newComment)
+    const db = client.db();
 
-    res.status(201).json({message: 'Added Comment', commemt: newComment})
+    const result = await db.collection("comments").insertOne(newComment);
+
+    console.log(result);
+
+    newComment.id = result.insertedId;
+
+    res.status(201).json({ message: "Added Comment", commemt: newComment });
   }
 
   if (req.method === "GET") {
+    const db = client.db();
 
-    const dummyList = [
-        {id: 'c1', name: 'Max', text: 'A first comment'},
-        {id: 'c2', name: 'Maxi', text: 'A second comment'},
-    ]
+    const documents = await db
+      .collection("comments")
+      .find()
+      .sort({ _id: -1 })
+      .toArray();
 
-    res.status(200).json({comments: dummyList})
-
+    res.status(200).json({ comments: documents });
   }
+
+  client.close();
 }
--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,16 +1,46 @@
-export default function handler(req, res) {
+import { MongoClient } from "mongodb";
 
-    if(req.method === 'POST') {
-        const userEmail = req.body.email
+async function connectDatabase() {
+    const client = await MongoClient.connect(
+        "mongodb+srv://maxmilian:asolole1234@clusters.vboii.mongodb.net/newsletter?retryWrites=true&w=majority"
+    );
 
-        if(!userEmail || !userEmail.includes('@')) {
-            res.status(422).json({message: 'Invalid email address.'})
-            
-            return
-        }
+    return client
+}
 
-        console.log(userEmail)
-        res.status(201).json({message : 'Signed Up!'})
+async function insertDocument(client, document) {
+    const db = client.db();
+
+    await db.collection("emails").insertOne({ document });
+}
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const userEmail = req.body.email;
+
+    if (!userEmail || !userEmail.includes("@")) {
+      res.status(422).json({ message: "Invalid email address." });
+
+      return;
     }
 
+    let client
+
+    try {
+        client = await connectDatabase()
+    } catch (error) {
+        res.status(500).json({message: 'Connecting to the database Failed!'})
+        return
+    }
+
+    try {
+        await insertDocument(client, {email: userEmail})
+        client.close()
+    } catch (error) {
+        res.status(500).json({message: 'Inserting data Failed!'})
+        return
+    }
+
+    res.status(201).json({ message: "Signed Up!" });
+  }
 }
