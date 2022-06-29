c7a3215c0eb73e3137ae534a82b078043412f862
AndyPew <christianandyp09@gmail.com>
2022-06-29 15:45:03 +0700

    notification

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,29 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-import Layout from '../components/layout/layout'
-import '../styles/globals.css'
+import Layout from "../components/layout/layout";
+import Notification from "../components/ui/notification";
+import { NotificationContextProvider } from "../store/notification-context";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return(
-  <Layout>
-    <Head>
-      <title>Next Events</title>
-      <meta name='description' content='NextJS Events'></meta>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width"></meta>
-    </Head>
-    <Component {...pageProps} />
-  </Layout>
-  )
+
+
+
+  return (
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <title>Next Events</title>
+          <meta name="description" content="NextJS Events"></meta>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          ></meta>
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </NotificationContextProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
